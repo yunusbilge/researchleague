19703e64489327f97e5e348028c7cd03461430e1
yunusbilge <36278593+yunusbilge@users.noreply.github.com>
2018-10-24 18:37:45 +0200

    length is changed again

--- recordingandplotting.m
+++ recordingandplotting.m
@@ -1,7 +1,7 @@
-% Record your voice for 10 seconds.
+% Record your voice for 5 seconds.
 recObj = audiorecorder;
 disp('Start speaking.')
-recordblocking(recObj, 10);
+recordblocking(recObj, 5);
 disp('End of Recording.');
 
 % Play back the recording.
