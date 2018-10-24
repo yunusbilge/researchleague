d06744062b0d696b69ea32aa1d0adaa9805882b4
yunusbilge <36278593+yunusbilge@users.noreply.github.com>
2018-10-24 18:32:36 +0200

    plotting is still bad

--- fft_trial1.m
+++ fft_trial1.m
@@ -1,4 +1,19 @@
-mycomplex = fft(myRecording);
-mycomplex_mag = abs(mycomplex);
-mycomplex_phase = angle(mycomplex);
-plot(mycomplex_mag);
+L=10;
+Fs=4000;
+
+mycomplex = fft(myRecording); %getting fft of my recording
+
+mycomplex_mag2 = abs(mycomplex); %getting two-sided spectrum
+
+mycomplex_phase = angle(mycomplex); %phase of the signal
+
+%computing the single-sided spectrum mycomplex_mag based on mycomplex_mag2 and the even-valued signal length L.
+mycomplex_mag = mycomplex_mag2(1:L/2+1); 
+mycomplex_mag(2:end-1) = 2*mycomplex_mag(2:end-1); 
+
+%plotting
+f = Fs*(0:(L/2))/L;
+plot(f,mycomplex_mag) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
