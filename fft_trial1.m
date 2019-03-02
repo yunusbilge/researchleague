a5045591184b8a7005e6c365f0d938ae41860d61
yunusbilge <36278593+yunusbilge@users.noreply.github.com>
2019-03-02 12:36:12 +0300

    The fft function works well on my recordings
    The length of the used singal is changed, then fft started to give the correct results to recorded sounds. Tested with online tone generator.

--- fft_trial1.m
+++ fft_trial1.m
@@ -1,19 +1,14 @@
 L=5000;  %length of the signal (miliseconds)
 Fs=8000;
-
-mycomplex = fft(myRecording); %getting fft of my recording
+n = 2^nextpow2(L);
+mycomplex = fft(myRecording,n); %getting fft of my recording
 
 mycomplex_mag2 = abs(mycomplex); %getting two-sided spectrum
 
 mycomplex_phase = angle(mycomplex); %phase of the signal
-
-%computing the single-sided spectrum mycomplex_mag based on mycomplex_mag2 and the even-valued signal length L.
-mycomplex_mag = mycomplex_mag2(1:L/2+1); 
-mycomplex_mag(2:end-1) = 2*mycomplex_mag(2:end-1); 
-
 %plotting
-f = Fs*(0:(L/2))/L;
-plot(f,mycomplex_mag) 
+f = Fs*(0:(n/2))/n;
+plot(f,mycomplex_mag2(1:n/2+1)) 
 title('Single-Sided Amplitude Spectrum of X(t)')
 xlabel('f (Hz)')
 ylabel('|P1(f)|')
